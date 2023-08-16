1fc074aa7bbea56db57cf7723dc0de116788ee00
İrem Çıngı <iremcingi@protonmail.com>
2023-08-16 17:09:45 +0300

    Create KotlinNumbers.kt

---
+++ KotlinNumbers.kt
@@ -0,0 +1,43 @@
+/*
+Name    Bit(Size)    Min Value              Max Value
+Long	    64	      -9,223,372,036,854…	    9,223,372,036,854…
+Int	      32	      -2,147,483,648	        2,147,483,647
+Short	    16	      -32,768	                32,767
+Byte	     8	      -128	                  127
+*/
+/*
+Name	    Bit (Size)	  Decimal Digit Precision
+Double	      64	              15-16
+Float	        32	               6-7
+*/
+
+println("The quotient of -9 and 3 is " + -9 / 3) 
+// Prints: The quotient of -9 and 3 is -3
+
+println("The product of 6 and 2, is ${6 * 2}") 
+// Prints: The product of 6 and 2 is 12
+
+/*
+PEMDAS
+1. Parentheses
+2. Multiplication
+3. Division
+4. Modulus
+5. Addition
+6. Subtraction
+
+You may have noticed that we skipped over the E (Exponents) in PEMDAS. 
+This is because Kotlin does not have an exponent operator and instead relies 
+on the .pow() function to calculate exponentiation. 
+*/
+
+/*
+Math.pow(5.0, 3.0) // 125.0
+Math.min(5, 3) // 3
+Math.random() // 0.3335735290527727
+Math.round(15.7) // 16
+Math.ceil(3.5) // 4.0
+Math.floor(6.9) // 6.0
+Math.abs(-20) // 20
+Math.sqrt(25.0) // 5.0
+*/
