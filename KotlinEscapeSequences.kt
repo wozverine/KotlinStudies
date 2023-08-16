1170f9466b319a26f373d0a9a90db45bc1358146
İrem Çıngı <iremcingi@protonmail.com>
2023-08-16 16:38:21 +0300

    Create KotlinString.kt

---
+++ KotlinEscapeSequences.kt
@@ -0,0 +1,13 @@
+/*
+\n Inserts a new line
+\t Inserts a tab
+\r Inserts a carriage return
+\' Inserts a single quote
+\" Inserts a double quote
+\\ Inserts a backslash
+\$ Inserts the dollar symbol
+*/
+
+//The readLine() function is used to accept a user’s input via the bash shell.
+//The built-in length property determines the number of characters in a String.
+//The built-in capitalize() function is used to capitalize the String onto which it’s appended.
